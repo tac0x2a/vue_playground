12de00b87534a8297b615ae71a43f5440e00c1be
TAC <tac@tac42.net>
2019-05-23 23:36:12 +0900

    add style/class, otehr app

--- main.js
+++ main.js
@@ -1,13 +1,29 @@
-var app = new Vue({
-    el: '#app',
-    data: {
+new Vue({
+    el: '#app', //# マウントする要素。html上の `<div id="app">` と対応する。
+
+    data: { //リアクティブデータ。変数と初期値。
         message: "Hello,Vue.js!",
         array: ["a", "b", "c"],
-        out: "Blank"
+        out: "Blank",
+        url: "https://www.tac42.net"
     },
-    methods: {
+
+    methods: { // イベントハンドラを書いたり、処理を分割するのにつかう
         handleClick: function (event) {
-            alert(event.target)
+            alert(this.out)
         }
+    }
+})
+
+new Vue({
+    el: '#app2', //# マウントする要素。html上の `<div id="app">` と対応する。
+
+    data: { //リアクティブデータ。変数と初期値。
+        message: "Hello,Vue.js 2!",
+        isChild: true,
+        isActive: true,
+        textColor: "blue",
+        bgColor: "green",
+        round: 10
     }
 })
